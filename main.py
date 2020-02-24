7a672da468d702e9b4c9ea1274d67e0a4967f453
SBlawski <48526640+SBlawski@users.noreply.github.com>
2020-02-23 16:04:09 -0800

    Added movable block, spawns example objs
    Move the green square using wsad, spawn red squares using space. Pretty basic.

--- main.py
+++ main.py
@@ -2,27 +2,53 @@ import pygame
 
 pygame.init()
 
+##  COLORS  ##
 WHITE = (255, 255, 255)
 RED = (255, 20, 20)
+BLUE = (0, 0, 255)
 
 size = (700, 500)
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption("Window")
 
+drawList = []
+
 running = True
 clock = pygame.time.Clock()
+
+block = pygame.Rect(300, 200, 20, 20)
 
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    screen.fill(WHITE)
-
-    pygame.draw.rect(screen, RED, [55, 200, 100, 70], 0)
-
-    pygame.display.update()
-
+        # if event.type == pygame.MOUSEBUTTONDOWN:
+        #     circ = pygame.mouse.get_pos()
+        #     pygame.draw.circle(screen, BLUE, circ, 30, 1)
     clock.tick(60)
+    screen.fill(WHITE)
+    pygame.event.pump()
+    keys = pygame.key.get_pressed()
+
+    if keys[pygame.K_a]:
+        block.x -= 3
+    if keys[pygame.K_d]:
+        block.x += 3
+    if keys[pygame.K_w]:
+        block.y -= 3
+    if keys[pygame.K_s]:
+        block.y += 3
+    if keys[pygame.K_SPACE]:
+        temp = [block.x + 5, block.y + 5]
+        tempBlock = pygame.Rect(temp, (10, 10))
+        drawList.append(tempBlock)
+
+
+    for obj in drawList:
+        pygame.draw.rect(screen, RED, obj)
+
+    pygame.draw.rect(screen, (150, 200, 20), block)
+    pygame.display.flip()
 
 pygame.quit()
